dc60b453fc78ceab183be1dcc3e0202de1fcb4bd
ked24 <kedmundson2000@icloud.com>
2025-11-25 13:29:19 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
-import { Routes, Route } from 'react-router-dom'; import Navbar from './components/Navbar'; import Footer from './components/Footer'; import Home from './pages/Home'; import Competitions from './pages/Competitions'; import HowItWorks from './pages/HowItWorks'; import Winners from './pages/Winners'; import About from './pages/About'; import Contact from './pages/Contact'; export default function App(){return (<div className='min-h-screen flex flex-col'><Navbar /><main className='flex-1 container mx-auto px-4 py-8'><Routes><Route path='/' element={<Home/>}/><Route path='/competitions' element={<Competitions/>}/><Route path='/how' element={<HowItWorks/>}/><Route path='/winners' element={<Winners/>}/><Route path='/about' element={<About/>}/><Route path='/contact' element={<Contact/>}/></Routes></main><Footer /></div>);}
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h2 style={{ padding: "1rem" }}>Welcome to The Kingdom Prize Co.</h2>
+    </div>
+  );
+}
+
+export default App;
